f0ee7b867fca129bac30e470635e5fd8279c9441
Joseph Safari <sajo2460hotmail.com>
2020-02-16 19:10:09 +0000

    Commit geo.gs index.html styles.css

--- geo.js
+++ geo.js
@@ -1,28 +1,39 @@
 var restaurants = [
     { 
+        name: "Tasquinha do Lagarto", address: "R. de Campalide 258, Lisboa",
+        lat: 38.734495, lon: -9.164574
+    },
+
+    { 
+        name: "Indian Dreams Benfica", address: "R. Olivéra Serpa 35D, 1500-356 Lisboa",
+        lat: 38.750077, lon: -9.205245
+    },
+
+    { 
+        name: "Boa Esperança", address: "Av. Gomes Pereira 3, 1500-328 Lisboa",
+        lat: 38.750276, lon: -9.197377
+    },
+    { 
+        name: "Costa do Malabar", address: "R. Rosa Damasceno 6, 1900-396 Lisboa",
+        lat: 38.735810, lon: -9.132050
+    },
+    
+    { 
+        name: "Restaurante Tonga", address: "Av. do Uruguai 26, 1500-613 Lisboa",
+        lat: 38.752308, lon: -9.196907
+    },
+
+    { 
+        name: "Restaurante Cantinho do Aziz", address: "Rua de sao lourenço nº5, 1100-530 Lisboa",
+        lat:38.714129, lon: -9.135654
+    },
+    
+    { 
         name: "Sushi Shop", address: "Via Po 160",
         lat: 41.9156915, lon: 12.471871
-    },
-    { 
-        name: "Gran Dragon", address: "Via Nomentana, 49",
-        lat: 41.9054717, lon: 12.4758192
-    },
-    { 
-        name: "Ale Carrete", address: "Via della Madonna dei Monti, 95",
-        lat: 41.8865606, lon: 12.4588248
-    },
-    { 
-        name: "Ristorante Scarpone", address: "Via di S. Pancrazio, 15",
-        lat: 41.8814485, lon: 12.4234625
-    },
-    { 
-        name: "Papetto", address: "Circonvallazione Gianicolense, 91",
-        lat: 41.864448, lon: 12.4203726
-    },
-    { 
-        name: "Ristorante Euro Bangla", address: "Via Eratostene, 44",
-        lat: 41.8741583, lon: 12.5150818
-    },
+    }
+
+    
 ];
 
 /* Calculate distance between two points in Earth in KM */
